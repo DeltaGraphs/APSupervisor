10bb2c5da0723344ed1b9c4268af4f7c3e48c069
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-06-08 11:48:25 +0200

    fix error

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ var server = http.createServer(app);
 var io = require('socket.io')(server);
 var request = require('request');
 
+
 // fix for Heroku polling error
-io.configure(function () {
+io.on('connection', function () {
 	io.set('transports', ['flashsocket', 'xhr-polling']);
 });
-
 /*
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
