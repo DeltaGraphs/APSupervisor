8d886ab33da6dd2a75b4e3b896da6aa08be86dd1
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-06-11 18:24:03 +0200

    restore

--- index.js
+++ index.js
@@ -477,14 +477,14 @@ var tableFlow_SIR1 = tableSIR1.createTableFlow({ID:'flowSIR1', name:'linea SIR1'
 
 
 function updateLinea(mapChartFlow,table,tableFlowName,linea){
-	var url = 'http://www.apsholding.it/index.php/informazioni/dov­e­il­mezzo­pubblico­in­tempo­reale?option=com_mappeaps&view=posmezzi&format=raw';
+/*	var url = 'http://www.apsholding.it/index.php/informazioni/dov­e­il­mezzo­pubblico­in­tempo­reale?option=com_mappeaps&view=posmezzi&format=raw';
 	var headers = { 
 	    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36',
 	    'Content-Type' : 'application/x-www-form-urlencoded',
-	    'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+//	    'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*//*;q=0.8',
 	    'Pragma': 'no-cache',
 	    'Origin': 'null',
-	    'Accept-Encoding': 'gzip, deflate',
+	    'Accept-Encoding': 'gzip',
 	    'Accept-Language': 'it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4',
 	    'Cache-Control': 'no-cache',
 	    'Connection': 'keep-alive',
@@ -528,7 +528,7 @@ function updateLinea(mapChartFlow,table,tableFlowName,linea){
 
 	req.on('error', function(err) {
 	   console.dir(err);
-	});
+	});*/
 }
 
 var poller=function(){
