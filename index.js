b6b1faa5d062880c994773a9386a8c7fc6345c41
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-06-11 21:45:45 +0200

    restore to working base

--- index.js
+++ index.js
@@ -480,54 +480,47 @@ function updateLinea(mapChartFlow,table,tableFlowName,linea){
 	makeRequest(linea);
 	function makeRequest(linea){
     
-	    var url = 'http://www.apsholding.it/index.php/informazioni/dov­e­il­mezzo­pubblico­in­tempo­reale?option=com_mappeaps&view=posmezzi&format=raw';
-		var headers = { 
-		    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36',
-		    'Content-Type' : 'application/x-www-form-urlencoded',
-		    'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-		    'Pragma': 'no-cache',
-		    'Origin': 'null',
-		    'Accept-Encoding': 'gzip',
-		    'Accept-Language': 'it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4',
-		    'Cache-Control': 'no-cache',
-		    'Connection': 'keep-alive',
-		    'gzip': true
-		};
+    var url = 'http://www.apsholding.it/index.php/informazioni/dov­e­il­mezzo­pubblico­in­tempo­reale?option=com_mappeaps&view=posmezzi&format=raw';
+	var headers = { 
+	    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36',
+	    'Content-Type' : 'application/x-www-form-urlencoded',
+	    'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	    'Pragma': 'no-cache',
+	    'Origin': 'null',
+	    'Accept-Encoding': 'gzip',
+	    'Accept-Language': 'it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4',
+	    'Cache-Control': 'no-cache',
+	    'Connection': 'keep-alive',
+	    'gzip': true
+	};
 
-		var options = {
-		    url: url,
-		    method: 'POST',
-		    headers: headers,
-		    form: { 'l': linea }
-		}
+	var options = {
+	    url: url,
+	    method: 'POST',
+	    headers: headers,
+	    form: { 'l': linea }
+	}
 
-		var response = request(options);
-	    gunzipJSON(response);
-	}
-	 
-	function gunzipJSON(response){
-	 	console.log('gunzipJSON');
-	    var gunzip = zlib.createGunzip();
-	    var json = "";
-	 
-	    gunzip.on('data', function(data){
-	        json += data.toString();
-	    });
-	        
-	    gunzip.on('end', function(){
-	    	console.log('FirstChar: '+json.charAt(0));
-	    	console.log(json);
-	    	if(json.charAt(0) !== '<'){
-		    	var obj=JSON.parse(json);
-		    	console.log(typeof obj);
-				mapChartFlow.updateMovie(obj);
-			}else{
-				console.log('ERRORE SULLA LINEA  : '+ linea)
-			}
-	    });
-	 
-	    response.pipe(gunzip);
-	}
+	var response = request(options);
+    console.log('RESPONSE');
+    gunzipJSON(response);
+}
+ 
+function gunzipJSON(response){
+ 	console.log('gunzipJSON');
+    var gunzip = zlib.createGunzip();
+    var json = "";
+ 
+    gunzip.on('data', function(data){
+        json += data.toString();
+    });
+        
+    gunzip.on('end', function(){
+    	console.dir(json);
+    });
+ 
+    response.pipe(gunzip);
+}
 }
 
 var poller=function(){
