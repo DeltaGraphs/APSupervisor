cb1f1759442cf4bf7359bd11efc9817f6b5c18ec
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-07-01 23:22:01 +0200

    restore default parameters of socket.io

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
-//var io = require('socket.io')(server);
+var io = require('socket.io')(server);
 
 
-    var io = require('socket.io')(server, {'transports': ['websocket', 'polling']});
+   // var io = require('socket.io')(server, {'transports': ['websocket', 'polling']});
     var request = require('request');
     var zlib = require('zlib');
 
